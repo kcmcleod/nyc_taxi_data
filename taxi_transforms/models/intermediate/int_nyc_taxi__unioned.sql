14832ec1ab30f15477c9d8fb7e7c1f998358eb5d
Kenneth McLeod <mcleod.kc@gmail.com>
2026-09-15 14:02:41 +0100

    [fix] improving duplicate row removal in the intermediate layer. different solutions for cloud and local dev. cloud not tested yet

--- taxi_transforms/models/intermediate/int_nyc_taxi__unioned.sql
+++ taxi_transforms/models/intermediate/int_nyc_taxi__unioned.sql
@@ -22,10 +22,24 @@ unioned_data as (
     from green_data
 )
 
-select 
-    {{ dbt_utils.generate_surrogate_key(['service_type', 'vendor_id', 'rate_code_id', 'pick_up_location_id', 'drop_off_location_id', 'payment_type_id', 'trip_type_id', 'pick_up_date_time', 'drop_off_date_time', 'fare_amount', 'total_amount', 'trip_distance_miles']) }} as taxi_trip_id,
-    ANY_VALUE(COLUMNS(*))
-from unioned_data
-group by taxi_trip_id
+
+   {% if target.name == 'dev' %}
+    -- set up for duckdb
+    select 
+        {{ dbt_utils.generate_surrogate_key(['service_type', 'vendor_id', 'rate_code_id', 'pick_up_location_id', 'drop_off_location_id', 'payment_type_id', 'trip_type_id', 'pick_up_date_time', 'drop_off_date_time', 'fare_amount', 'total_amount', 'trip_distance_miles']) }} as taxi_trip_id,
+        ANY_VALUE(COLUMNS(*))
+    from unioned_data
+    group by taxi_trip_id -- stops dups
+   {% else %}
+     -- set up for analytics awarehouse
+        select 
+        {{ dbt_utils.generate_surrogate_key(['service_type', 'vendor_id', 'rate_code_id', 'pick_up_location_id', 'drop_off_location_id', 'payment_type_id', 'trip_type_id', 'pick_up_date_time', 'drop_off_date_time', 'fare_amount', 'total_amount', 'trip_distance_miles']) }} as taxi_trip_id,
+        *
+    from unioned_data
+    qualify row_number() over (partition by taxi_trip_id order by pick_up_date_time desc) = 1
+   {% endif %}
+
+
+
 
 
